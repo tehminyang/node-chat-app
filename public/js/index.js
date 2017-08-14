46a3624c7d7d6a0d3a62ff1a0259ef949c0108e0
Tim Yang <timyang@tim-yang-mba.local>
2017-08-13 23:48:30 -0700

    Emite newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        to: 'sab',
-        text: 'Yo, its me'
-    })
 });
 
 socket.on('disconnect', function() {
