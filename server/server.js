fb88e4d55ee12b479e32b3172566afb120605318
Tim Yang <timyang@tim-yang-mba.local>
2017-08-13 23:58:25 -0700

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,9 +15,15 @@ io.on('connection', (socket) => {
     console.log('New user connected');
 
     socket.emit('newMessage', {
-        from: 'tim',
-        text: 'WHASSUP',
-        createdAt: 123
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
     });
 
     socket.on('createMessage', (message) => {
