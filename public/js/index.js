21388f02b5bb7e08e855d7371346675d528bc8cc
Tim Yang <timyang@tim-yang-mba.local>
2017-08-15 18:37:13 -0700

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -31,15 +31,16 @@ socket.on('newLocationMessage', function (message) {
 //     console.log('Got it ', data);
 // });
 
+var messageTextBox = jQuery('[name="message"]');
+
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message').val()
+        text: messageTextBox.val()
     }, function () {
-
+       messageTextBox.val('');
     });
-    jQuery('[name="message"]').val('');
 });
 
 var locationButton = jQuery('#send-location');
@@ -47,12 +48,15 @@ locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not suppoted by your browser.');
     }
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled');
         alert('Unable to fetch location.');
     });
 })
